85d1fc88772ae7dea0c073417d703adcd82b5599
Martin Pino Cornejo <martin.pino@sansano.usm.cl>
2024-11-27 01:36:40 -0300

    Update solucion_inicial.cpp

--- src/solucion_inicial.cpp
+++ src/solucion_inicial.cpp
@@ -1,58 +1,118 @@
 #include "solucion_inicial.h"
+#include "utils.h"
 #include <algorithm>
 #include <random>
 #include <set>
+#include <ostream>
+#include <iostream>
 
 using namespace std;
 
 vector<int> generarSolucionInicial(const InstanciaTUP& instancia, int N, int d1, int d2, unsigned int semilla) {
-    int numRondas = 4 * N - 2;
+    int numRondas = instancia.matrizRondas.size(); // Rondas totales
     int numEquipos = 2 * N;
+    int numJuegosPorRonda = numEquipos / 2;
+
     vector<int> solucionInicial(N * numRondas, -1);
 
     mt19937 g(semilla);
-    vector<int> equipos(numEquipos);
-    iota(equipos.begin(), equipos.end(), 0);
 
+    // Crear lista de juegos por ronda
+    vector<vector<int>> juegosPorRonda(numRondas);
+    for (int ronda = 0; ronda < numRondas; ++ronda) {
+        // Cada ronda tiene numJuegosPorRonda juegos
+        for (int juego = 0; juego < numJuegosPorRonda; ++juego) {
+            juegosPorRonda[ronda].push_back(juego);
+        }
+    }
+
+    // Asignar árbitros a juegos en cada ronda
     for (int arbitro = 0; arbitro < N; ++arbitro) {
-        vector<int> rondas(numRondas, -1);
-        int consecutivasSede = 0;
-        int consecutivasEquipo = 0;
-        int ultimoEquipo = -1;
+        int consecutivasSede = 1;
+        int consecutivasEquipo = 1;
         int ultimaSede = -1;
+        int ultimoEquipo = -1;
 
         for (int ronda = 0; ronda < numRondas; ++ronda) {
+            vector<int>& juegosDisponibles = juegosPorRonda[ronda];
 
-            shuffle(equipos.begin(), equipos.end(), g);
+            // Desordenar los juegos disponibles
+            shuffle(juegosDisponibles.begin(), juegosDisponibles.end(), g);
 
-            for (int equipo : equipos) {
-                int sede = equipo;
+            bool asignado = false;
+            for (int juego : juegosDisponibles) {
+                int equipoLocal, equipoVisitante;
+                obtenerEquiposDelJuego(instancia, ronda, juego, equipoLocal, equipoVisitante);
+                int sede = equipoLocal; // La sede es el equipo local
 
+                // Verificar restricciones de sede y equipo
                 bool cumpleRestricciones = true;
                 if (sede == ultimaSede && consecutivasSede >= (N - d1)) {
                     cumpleRestricciones = false;
                 }
-                if (equipo == ultimoEquipo && consecutivasEquipo >= ((N / 2) - d2)) {
+
+                // Verificar para ambos equipos (local y visitante)
+                bool mismoEquipo = (equipoLocal == ultimoEquipo) || (equipoVisitante == ultimoEquipo);
+                if (mismoEquipo && consecutivasEquipo >= ((N / 2) - d2)) {
                     cumpleRestricciones = false;
                 }
 
                 if (cumpleRestricciones) {
-                    rondas[ronda] = equipo;
+                    // Asignar el juego al árbitro
+                    solucionInicial[arbitro * numRondas + ronda] = juego;
+
+                    // Actualizar variables
                     if (sede == ultimaSede) consecutivasSede++;
-                    else consecutivasSede = 1;
+                    else {
+                        consecutivasSede = 1;
+                        ultimaSede = sede;
+                    }
 
-                    if (equipo == ultimoEquipo) consecutivasEquipo++;
-                    else consecutivasEquipo = 1;
+                    if (equipoLocal == ultimoEquipo || equipoVisitante == ultimoEquipo) {
+                        consecutivasEquipo++;
+                    } else {
+                        consecutivasEquipo = 1;
+                        // Puedes elegir actualizar a cualquiera de los dos equipos
+                        ultimoEquipo = equipoLocal; // O equipoVisitante
+                    }
 
-                    ultimaSede = sede;
-                    ultimoEquipo = equipo;
+                    // Remover el juego de los disponibles
+                    juegosDisponibles.erase(remove(juegosDisponibles.begin(), juegosDisponibles.end(), juego), juegosDisponibles.end());
+                    asignado = true;
                     break;
                 }
             }
-        }
 
-        for (int ronda = 0; ronda < numRondas; ++ronda) {
-            solucionInicial[arbitro * numRondas + ronda] = rondas[ronda];
+            if (!asignado) {
+                // No se pudo asignar un juego cumpliendo restricciones
+                // Asignar cualquier juego disponible
+                if (!juegosDisponibles.empty()) {
+                    int juego = juegosDisponibles.back();
+                    juegosDisponibles.pop_back();
+
+                    // Asignar el juego al árbitro
+                    solucionInicial[arbitro * numRondas + ronda] = juego;
+
+                    // Actualizar variables
+                    int equipoLocal, equipoVisitante;
+                    obtenerEquiposDelJuego(instancia, ronda, juego, equipoLocal, equipoVisitante);
+                    int sede = equipoLocal;
+
+                    if (sede == ultimaSede) consecutivasSede++;
+                    else {
+                        consecutivasSede = 1;
+                        ultimaSede = sede;
+                    }
+
+                    // Actualizar el último equipo (puedes elegir local o visitante)
+                    ultimoEquipo = equipoLocal; // O equipoVisitante
+                    consecutivasEquipo = 1; // Reinicia el contador
+                } else {
+                    // No hay juegos disponibles en esta ronda, esto no debería ocurrir
+                    cerr << "Error: No hay juegos disponibles para asignar al árbitro " << arbitro << " en la ronda " << ronda << endl;
+                    return {}; // Retornar solución vacía o manejar el error según convenga
+                }
+            }
         }
     }
 
