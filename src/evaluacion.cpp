c72538f4c590a6a005c3fa5963abcf095d09d47d
Martin Pino Cornejo <martin.pino@sansano.usm.cl>
2024-11-27 01:36:37 -0300

    Update evaluacion.cpp

--- src/evaluacion.cpp
+++ src/evaluacion.cpp
@@ -1,4 +1,5 @@
 #include "evaluacion.h"
+#include "utils.h" 
 
 using namespace std;
 
@@ -8,10 +9,29 @@ int evaluarSolucion(const vector<int>& solucion, const InstanciaTUP& instancia) 
     int numArbitros = instancia.numArbitros;
 
     for (int arbitro = 0; arbitro < numArbitros; ++arbitro) {
+        int idx = arbitro * numRondas;
+        int gameAnterior = solucion[idx];
+        int rondaAnterior = 0;
+
+        // Obtener la sede anterior
+        int equipoLocalAnterior, equipoVisitanteAnterior;
+        obtenerEquiposDelJuego(instancia, rondaAnterior, gameAnterior, equipoLocalAnterior, equipoVisitanteAnterior);
+        int sedeAnterior = equipoLocalAnterior;
+
         for (int ronda = 1; ronda < numRondas; ++ronda) {
-            int sedeActual = solucion[arbitro * numRondas + ronda - 1];
-            int sedeSiguiente = solucion[arbitro * numRondas + ronda];
-            distanciaTotal += instancia.matrizDistancias[sedeActual][sedeSiguiente];
+            int idxActual = arbitro * numRondas + ronda;
+            int gameActual = solucion[idxActual];
+
+            // Obtener la sede actual
+            int equipoLocalActual, equipoVisitanteActual;
+            obtenerEquiposDelJuego(instancia, ronda, gameActual, equipoLocalActual, equipoVisitanteActual);
+            int sedeActual = equipoLocalActual;
+
+            // Sumar la distancia entre las sedes
+            distanciaTotal += instancia.matrizDistancias[sedeAnterior][sedeActual];
+
+            // Actualizar sede anterior
+            sedeAnterior = sedeActual;
         }
     }
 
